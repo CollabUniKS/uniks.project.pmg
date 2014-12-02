10bac21b6c21b684a8ad411ddbc2253823f90893
Stefan Werner <s.werner@stewerks.de>
2014-12-02 19:29:51 +0100

    hiworld

--- HiWorld/src/com/example/hiworld/MainActivity.java
+++ HiWorld/src/com/example/hiworld/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.hiworld;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+	
+	private SensorManager sensorManager;
+	private boolean color = false;
+	private View view;
+	private long lastUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = findViewById(R.id.textView1);
+        view.setBackgroundColor(Color.GREEN);
+        
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lastUpdate = System.currentTimeMillis();
     }
 
 
@@ -33,4 +49,63 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+		      getAccelerometer(event);
+		    }
+		
+	}
+
+	private void getAccelerometer(SensorEvent event) {
+	    float[] values = event.values;
+	    // Movement
+	    float x = values[0];
+	    float y = values[1];
+	    float z = values[2];
+
+	    float accelationSquareRoot = (x * x + y * y + z * z)
+	        / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+	    long actualTime = event.timestamp;
+	    if (accelationSquareRoot >= 2) //
+	    {
+	      if (actualTime - lastUpdate < 200) {
+	        return;
+	      }
+	      lastUpdate = actualTime;
+	      Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT)
+	          .show();
+	      if (color) {
+	        view.setBackgroundColor(Color.GREEN);
+	      } else {
+	        view.setBackgroundColor(Color.RED);
+	      }
+	      color = !color;
+	    }
+	  }
+	
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	  protected void onResume() {
+	    super.onResume();
+	    // register this class as a listener for the orientation and
+	    // accelerometer sensors
+	    sensorManager.registerListener(this,
+	        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+	        SensorManager.SENSOR_DELAY_NORMAL);
+	  }
+
+	  @Override
+	  protected void onPause() {
+	    // unregister listener
+	    super.onPause();
+	    sensorManager.unregisterListener(this);
+	  }
 }
---
+++ helloWorld/gen/example/hw/uniks/project/pmg/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package example.hw.uniks.project.pmg;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
